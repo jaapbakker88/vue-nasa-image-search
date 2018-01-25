bd570a224a280480071be097ed9c05911d52881b
Jaap Bakker <hello@bakkerlabs.nl>
2018-01-25 12:42:40 +0100

    initial commit

---
+++ src/components/Search.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="search">
+    <form v-on:keyup.prevent="getPics()">
+      <input type="text" placeholder="Search NASA pictures" v-model="query" />
+    </form>
+    <!-- <button v-on:click="count ++">click me ({{ count }})</button> -->
+    <div class="results" v-if="results">
+      <div v-for="result in results" class="img-result">
+        <img v-bind:src="result.links[0].href" />
+        <p>{{result.data[0].title}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'search',
+  data () {
+    return {
+      query: '',
+      results: '',
+      count: 0
+    }
+  },
+  methods: {
+    getPics(){
+      axios.get(`https://images-api.nasa.gov/search?q=${this.query}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+input[type="text"] {
+  max-width: 960px;
+  width: 100%;
+  padding: 8px;
+  /*font-size: 16px;*/
+}
+
+.results {
+  max-width: 960px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.img-result {
+  position: relative;
+  margin: 1em;
+  width: 200px;
+  height: 200px;
+  float: left;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+}
+.img-result p {
+  width: 100%;
+  z-index: 10;
+  padding: 8px;
+  margin: 0;
+  color: white;
+  text-align: center;
+  text-shadow: 0px 1px 3px rgba(0,0,0,0.4);
+  background: transparent;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.5));  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.5)); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  
+}
+.img-result img {
+  position:absolute;
+  width: inherit; height: inherit;
+  object-fit: cover;
+}
+</style>
